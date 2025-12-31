c99e085a85d328bd17b001b6ed200c06a6f134ae
andreitd <andrei.toderascu.office@gmail.com>
2025-12-31 17:44:34 +0200

    again

--- java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
+++ java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
@@ -270,7 +270,7 @@ public class SchemeBuffer extends Npc
 				final String costText = (cost > 0) ? " - cost: " + NumberFormat.getInstance(Locale.ENGLISH).format(cost) : "";
 
 				sb.append("<table width=280 cellpadding=0 cellspacing=0>");
-                sb.append("</tr><img src=\"L2UI.SquareGray\" width=277 height=1>");
+                sb.append("<img src=\"L2UI.SquareGray\" width=277 height=1>");
 				sb.append("<tr><td height=10></td></tr>");
 				sb.append("<tr><td align=center>");
 				sb.append("<table cellpadding=0 cellspacing=0><tr><td height=8></td></tr></table>");

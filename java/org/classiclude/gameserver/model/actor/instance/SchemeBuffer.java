82a4ab357a11217b6c068f1ccfdad6336ee6e25a
andreitd <andrei.toderascu.office@gmail.com>
2025-12-31 12:27:06 +0200

    scheme buffer with buttons instead of a

--- java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
+++ java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
@@ -271,8 +271,10 @@ public class SchemeBuffer extends Npc
 				sb.append("<a action=\"bypass -h npc_%objectId%_givebuffs;" + scheme.getKey() + ";" + cost + ";pet\">Use on Pet</a>&nbsp;|&nbsp;");
 				sb.append("<a action=\"bypass npc_%objectId%_editschemes;Buffs;" + scheme.getKey() + ";1\">Edit</a>&nbsp;|&nbsp;");
 				sb.append("<a action=\"bypass npc_%objectId%_deletescheme;" + scheme.getKey() + "\">Delete</a><br>");
-				sb.append("<td><button value=\"Create\" action=\"bypass npc_%objectId%_createscheme $name\" width=65 height=21 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_CT1.Button_DF\"></td>&nbsp;|&nbsp;");
-//                 sb.append("<td><button value="Delete" action="bypass npc_%objectId%_createscheme $name" width=65 height=21 back="L2UI_CT1.Button_DF_Down" fore="L2UI_CT1.Button_DF"><a action=\"bypass npc_%objectId%_deletescheme;" + scheme.getKey() + "\">Delete</a></td><br>");
+				sb.append("<button value=\"Use on Me\" action=\"bypass -h npc_%objectId%_givebuffs;" + scheme.getKey() + ";" + cost + "\" width=65 height=21 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_CT1.Button_DF\">&nbsp;|&nbsp;");
+				sb.append("<button value=\"Use on Pet\" action=\"bypass -h npc_%objectId%_givebuffs;" + scheme.getKey() + ";" + cost + ";pet\" width=65 height=21 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_CT1.Button_DF\">&nbsp;|&nbsp;");
+				sb.append("<button value=\"Edit\" action=\"bypass npc_%objectId%_editschemes;Buffs;" + scheme.getKey() + ";1\" width=65 height=21 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_CT1.Button_DF\">&nbsp;|&nbsp;");
+				sb.append("<button value=\"Delete\" action=\"bypass npc_%objectId%_deletescheme;" + scheme.getKey() + "\" width=65 height=21 back=\"L2UI_CT1.Button_DF_Down\" fore=\"L2UI_CT1.Button_DF\">&nbsp;|&nbsp;");
 			}
 		}
 		

9ddbce4af90c5f1050d4a6d4cc6f3c62ae7b7c2f
andreitd <andrei.toderascu.office@gmail.com>
2026-01-02 22:03:57 +0200

    try confirm with null

--- java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
+++ java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
@@ -219,7 +219,7 @@ public class SchemeBuffer extends Npc
 		}
 		else if (currentCommand.startsWith("deletescheme"))
 		{
-            player.setAdminConfirmCmd(fullCommand);
+            player.setAdminConfirmCmd(null);
             final ConfirmDlg dlg = new ConfirmDlg(SystemMessageId.S1_3);
             dlg.getSystemMessage().addString("Are you sure you want delete the scheme?");
 			try

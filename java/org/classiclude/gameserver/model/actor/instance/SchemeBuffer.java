e263aa6bc2675ddff4f4c9fa5818105c76469e49
andreitd <andrei.toderascu.office@gmail.com>
2026-01-03 19:15:44 +0200

    revert individual buffs

--- java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
+++ java/org/classiclude/gameserver/model/actor/instance/SchemeBuffer.java
@@ -31,18 +31,14 @@ import java.util.StringTokenizer;
 import org.classiclude.Config;
 import org.classiclude.gameserver.data.SchemeBufferTable;
 import org.classiclude.gameserver.data.xml.SkillData;
-import org.classiclude.gameserver.model.actor.Creature;
 import org.classiclude.gameserver.model.actor.Npc;
 import org.classiclude.gameserver.model.actor.Player;
 import org.classiclude.gameserver.model.actor.Summon;
 import org.classiclude.gameserver.model.actor.templates.NpcTemplate;
 import org.classiclude.gameserver.model.skill.Skill;
 import org.classiclude.gameserver.network.serverpackets.NpcHtmlMessage;
-import org.classiclude.gameserver.network.serverpackets.MagicSkillUse;
 import org.classiclude.gameserver.util.MathUtil;
 import org.classiclude.gameserver.util.Util;
-
-
 
 public class SchemeBuffer extends Npc
 {
@@ -105,58 +101,6 @@ public class SchemeBuffer extends Npc
 		{
 			showSchemeBuffsWindow(player);
 		}
-        else if (currentCommand.startsWith("invidualbuffs"))
-        {
- 		    showIndividualBuffsWindow(player);
-        }
-        else if (currentCommand.startsWith("giveindividual"))
-        {
-            final String fullBypass = command.replace("giveindividual;", "");
-            final String[] buypassOptions = fullBypass.split(";");
-            final int buffCount = buypassOptions.length - 1;
-//             final String page = buypassOptions[buffCount];
-            if (player.getInventory().getInventoryItemCount(Config.COMMUNITYBOARD_CURRENCY, -1) < (Config.COMMUNITYBOARD_BUFF_PRICE * buffCount))
-            {
-            	player.sendMessage("Not enough currency!");
-            }
-            else
-            {
-            	player.destroyItemByItemId("CB_Buff", Config.COMMUNITYBOARD_CURRENCY, Config.COMMUNITYBOARD_BUFF_PRICE * buffCount, player, true);
-            	final Pet pet = player.getPet();
-            	final List<Creature> targets = new ArrayList<>(4);
-            	targets.add(player);
-            	if (pet != null)
-            	{
-            		targets.add(pet);
-            	}
-
-            	player.getServitors().values().forEach(targets::add);
-
-            	for (int i = 0; i < buffCount; i++)
-            	{
-            		final Skill skill = SkillData.getInstance().getSkill(Integer.parseInt(buypassOptions[i].split(",")[0]), Integer.parseInt(buypassOptions[i].split(",")[1]));
-            		if (!Config.COMMUNITY_AVAILABLE_BUFFS.contains(skill.getId()))
-            		{
-            			continue;
-            		}
-            		for (Creature target : targets)
-            		{
-            			if (skill.isSharedWithSummon() || target.isPlayer())
-            			{
-            				skill.applyEffects(player, target);
-            				if (Config.COMMUNITYBOARD_CAST_ANIMATIONS)
-            				{
-            					player.sendPacket(new MagicSkillUse(player, target, skill.getId(), skill.getLevel(), skill.getHitTime(), skill.getReuseDelay()));
-            					// not recommend broadcast
-            					// player.broadcastPacket(new MagicSkillUse(player, target, skill.getId(), skill.getLevel(), skill.getHitTime(), skill.getReuseDelay()));
-            				}
-            			}
-            		}
-            	}
-            }
-
-            showIndividualBuffsWindow(player);
-        }
 		else if (currentCommand.startsWith("givebuffs"))
 		{
 			final String schemeName = st.nextToken();
@@ -190,7 +134,6 @@ public class SchemeBuffer extends Npc
 		{
 			showEditSchemeWindow(player, st.nextToken(), st.nextToken(), Integer.parseInt(st.nextToken()));
 		}
-
 		else if (currentCommand.startsWith("skill"))
 		{
 			final String groupType = st.nextToken();
@@ -305,17 +248,6 @@ public class SchemeBuffer extends Npc
 		}
 		return "data/html/mods/SchemeBuffer/" + filename + ".htm";
 	}
-
-    private void showIndividualBuffsWindow(Player player)
-    {
-        final NpcHtmlMessage html = new NpcHtmlMessage(getObjectId());
-    	html.setFile(player, getHtmlPath(getId(), 3, player));
-//     	html.replace("%schemes%", sb.toString());
-//    		html.replace("%max_schemes%", Config.BUFFER_MAX_SCHEMES);
-//    		html.replace("%objectId%", getObjectId());
-   		player.sendPacket(html);
-    }
-
 
 	/**
 	 * Sends an html packet to player with Give Buffs menu info for player and pet, depending on targetType parameter {player, pet}

8f3e9587bd51a9b2e8320bb01183b1794ad2c1d3
andreitd <andrei.toderascu.office@gmail.com>
2026-01-05 11:26:47 +0200

    add second sell?

--- dist/game/data/scripts/handlers/communityboard/HomeBoard.java
+++ dist/game/data/scripts/handlers/communityboard/HomeBoard.java
@@ -187,7 +187,16 @@ public class HomeBoard implements IParseBoardHandler
             returnHtml = HtmCache.getInstance().getHtm(player, "data/html/CommunityBoard/Custom/" + page + ".html");
             player.sendPacket(new BuyList(BuyListData.getInstance().getBuyList(423), player, 0));
             player.sendPacket(new ExBuySellList(player, false));
-		}
+		} else if (command.startsWith("_bbsexcmultisell"))
+        {
+              player.sendMessage("line 196");
+            final String fullBypass = command.replace("_bbsexcmultisell;", "");
+            final String[] buypassOptions = fullBypass.split(",");
+            final int multisellId = Integer.parseInt(buypassOptions[0]);
+            final String page = buypassOptions[1];
+            returnHtml = HtmCache.getInstance().getHtm(player, "data/html/CommunityBoard/Custom/" + page + ".html");
+            MultisellData.getInstance().separateAndSend(multisellId, player, null, true);
+        }
 
 
 

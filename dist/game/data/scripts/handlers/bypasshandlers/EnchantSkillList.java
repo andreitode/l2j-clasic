3f20d976e5e6176dfb95d8ef56345f26099676f0
andreitd <andrei.toderascu.office@gmail.com>
2026-01-07 20:00:10 +0200

    break by ; to get params

--- dist/game/data/scripts/handlers/bypasshandlers/EnchantSkillList.java
+++ dist/game/data/scripts/handlers/bypasshandlers/EnchantSkillList.java
@@ -174,13 +174,10 @@ public class EnchantSkillList implements IBypassHandler
 	public String showSkillDetailsWithNoNpc(Player player, String command)
     {
         player.sendMessage(command);
-        String[] args = command.split(" ");
+        String[] args = command.split(";");
         int skillId = Integer.parseInt(args[1]);
         int routeId = Integer.parseInt(args[2]);
 
-        player.sendMessage("line 181 in showSkillDetailsWithNoNpc");
-        player.sendMessage(String.valueOf(skillId));
-        player.sendMessage(String.valueOf(routeId));
         Skill skill = player.getKnownSkill(skillId);
         if (skill == null)
         {

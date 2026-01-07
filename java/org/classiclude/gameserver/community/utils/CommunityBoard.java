667eb504ff59a719a0be6bc35f2c48ffd8af0097
andreitd <andrei.toderascu.office@gmail.com>
2026-01-07 15:46:13 +0200

    skill enchant add html

--- java/org/classiclude/gameserver/community/utils/CommunityBoard.java
+++ java/org/classiclude/gameserver/community/utils/CommunityBoard.java
@@ -87,6 +87,17 @@ public final class CommunityBoard
         	buttons.add(btn);
         }
 
+        if (Config.COMMUNITYBOARD_ENABLE_AUGMENT)
+        {
+            btn = links_template;
+
+            btn = btn.replace("%text%", "Augment");
+            btn = btn.replace("%iconBack%", "L2UI_CT1.OlympiadWnd_DF_Reward_Down");
+            btn = btn.replace("%iconFore%", "L2UI_CT1.OlympiadWnd_DF_Reward");
+            btn = btn.replace("%action%", "bypass _bbsaugment");
+            buttons.add(btn);
+        }
+
         if (Config.COMMUNITYBOARD_ENABLE_SKILL_ENCHANT)
         {
             btn = links_template;
@@ -98,6 +109,7 @@ public final class CommunityBoard
 
             buttons.add(btn);
         }
+
 		for (String button : buttons)
 		{
 			links += button;

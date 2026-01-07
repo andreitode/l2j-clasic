3dc99e6e9d14df1e694a7f8a718e728867d92436
andreitd <andrei.toderascu.office@gmail.com>
2026-01-07 19:55:31 +0200

    typo

--- java/org/classiclude/gameserver/network/serverpackets/ExEnchantSkillList.java
+++ java/org/classiclude/gameserver/network/serverpackets/ExEnchantSkillList.java
@@ -82,7 +82,7 @@ public class ExEnchantSkillList
 	    returnHtml = HtmCache.getInstance().getHtm(player, "data/html/CommunityBoard/Custom/skillenchant/skill_list.html");
 // 	    returnHtml = returnHtml.replace("%objectId%", npc.getObjectId());
         returnHtml = returnHtml.replace("%skill_enchant_list%", getSkillEnchantListHtmlWithNoNpc(player));
-        returnHtml = returnHtml.replace("%paging%", getPagingHtmlWNoNpc(player));
+        returnHtml = returnHtml.replace("%paging%", getPagingHtmlWithNoNpc(player));
 
        return returnHtml;
 	}

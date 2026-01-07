2880132798854b7c9bdd26ea4d07dbe5f7c377e7
andreitd <andrei.toderascu.office@gmail.com>
2026-01-07 17:07:44 +0200

    make it public

--- java/org/classiclude/gameserver/model/actor/instance/Folk.java
+++ java/org/classiclude/gameserver/model/actor/instance/Folk.java
@@ -127,7 +127,7 @@ public class Folk extends Npc
 	 * This method displays EnchantSkillList to the player.
 	 * @param player The player who requested the method.
 	 */
-	public void showEnchantSkillList(Player player)
+	public static void showEnchantSkillList(Player player)
 	{
 		if (!player.isInCategory(CategoryType.FOURTH_CLASS_GROUP))
 		{
